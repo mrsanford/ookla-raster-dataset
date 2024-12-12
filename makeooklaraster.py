11041a347061115eefec2fb10cd83f567cc1bbd4
mrsanford <158346002+mrsanford@users.noreply.github.com>
2024-12-12 10:27:42 -0500

    consolidating the read_parquet function in the make ookla raster file to eliminate the need to import it from another function

--- makeooklaraster.py
+++ makeooklaraster.py
@@ -1,16 +1,59 @@
-# Extra Questions
-## I have a script that converts the geoparquet to a geopackage, which can be opened in QGIS, but other than the graphical benefit, is there a benefit to this code?
-
 # imports
-import geopandas as gpd
+from ookladatadownload import *
 import numpy as np
-from pathlib import Path
+import pandas as pd
+import geopandas as gpd
+from shapely import wkt
+from shapely.wkt import loads
+
 import rasterio
-# importing function from my other file
-from geoparquettogpkg import read_parquet
+from rasterio.crs import CRS
 from pyquadkey2 import QuadKey
 from affine import Affine
-from rasterio.crs import CRS
+
+import logging
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+geoparquet_dir = Path('/Users/michellesanford/GitHub/geo-datasets/datasets/ookla_speedtest')
+test_parquet_file = '2019-01-01_performance_fixed_tiles.parquet'
+
+def read_parquet(parquet_path: str):
+    if parquet_path.exists():
+        logger.info(f'Reading Parquet file: {parquet_path}')
+        parquet_data = pd.read_parquet(parquet_path)
+        parquet_data["geometry"] = gpd.GeoSeries.from_wkt(parquet_data["tile"])
+        gdf = gpd.GeoDataFrame(parquet_data)
+        logger.info(parquet_data.head())
+        return gdf
+    else:
+        logger.warning(f'Parquet file not found: {parquet_path}')
+        return None
+
+def make_geopackage(gdf, output_path):
+    if gdf is not None and isinstance(gdf, gpd.GeoDataFrame):
+        try:
+            gdf.to_file(output_path, driver='GPKG')
+            logger.info(f'Saved data as GeoPackage: {output_path}')
+        except Exception as e:
+            logger.error(f'Failed to save GeoPackage: {e}')
+    else:
+        logger.warning('Invalid GeoDataFrame provided to make_geopackage.')
+
+def process_geodata():
+    parquet_path = geoparquet_dir / test_parquet_file
+    gdf = read_parquet(parquet_path)
+    if gdf is not None:
+        output_path = geoparquet_dir / '2019-01-01_performance_fixed_tiles.gpkg'
+        print('hi')
+        make_geopackage(gdf, output_path)
+    else:
+        logger.error('Failed to read Parquet data or create GeoDataFrame.')
+
+if __name__ == "__main__":
+    process_geodata()
 
 # reading in data
 parquet_data_path = Path('/Users/michellesanford/GitHub/geo-datasets/datasets/ookla_speedtest/2019-01-01_performance_fixed_tiles.parquet')
