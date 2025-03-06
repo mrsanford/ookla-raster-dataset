438eae1f27f4b716cc3da7a0e65d242405e866b5
Michelle Sanford <mrsanford@wm.edu>
2025-03-06 11:58:08 -0500

    updating naming conventions

--- src/dataset_download.py
+++ src/dataset_download.py
@@ -4,14 +4,14 @@ import logging
 from botocore import UNSIGNED
 from botocore.client import BaseClient
 from botocore.config import Config
-from src.helpers import LOCAL_DIRECTORY, BUCKET_NAME, REGION_NAME, QUARTERS
+from src.helpers import GEOPARQUET_DIR, BUCKET_NAME, REGION_NAME, QUARTERS
 
 # configuring the logger/error handling
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # making the directory
-os.makedirs(LOCAL_DIRECTORY, exist_ok=True)
+os.makedirs(GEOPARQUET_DIR, exist_ok=True)
 
 
 # creating the s3 client with anonymous token
@@ -22,13 +22,15 @@ def create_s3_client() -> BaseClient:
 
 
 # creating the download file function and creating the loggers
-def download_file(s3_client: BaseClient, s3_key: str, local_file_path: str) -> None:
+def prepare_download(
+    s3_client: BaseClient, s3_key: str, local_file_path: str = GEOPARQUET_DIR
+) -> None:
     if os.path.exists(local_file_path):
         logger.info(f"File {local_file_path} already exists. Skipping download.")
         return
     logger.info(f"Downloading s3://{BUCKET_NAME}/{s3_key} to {local_file_path}")
     try:
-        s3_client.download_file(BUCKET_NAME, s3_key, local_file_path)
+        s3_client.prepare_download(BUCKET_NAME, s3_key, local_file_path)
         logger.info(f"Successfully downloaded {local_file_path}")
     except Exception as e:
         logger.error(f"Error downloading s3://{BUCKET_NAME}/{s3_key}: {e}")
@@ -50,10 +52,5 @@ def download_files() -> None:
                     filename = f"{year}-{month}-01_performance_{service_type}_tiles"
                     filename += ".zip" if format_type == "shapefiles" else ".parquet"
                     s3_key = f"{format_type}/performance/type={service_type}/year={year}/quarter={quarter}/{filename}"
-                    local_file_path = os.path.join(LOCAL_DIRECTORY, filename)
-                    download_file(s3_client, s3_key, local_file_path)
-
-
-# calling the function
-if __name__ == "__main__":
-    download_files()
+                    local_file_path = os.path.join(GEOPARQUET_DIR, filename)
+                    prepare_download(s3_client, s3_key, local_file_path)
