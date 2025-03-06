25989198c03957ff12a20af9de945c5901393787
Michelle Sanford <mrsanford@wm.edu>
2025-03-06 12:30:25 -0500

    resolving botocore checksum spam bc its annoying and changing the file paths to be accurate and output correctly

--- src/dataset_download.py
+++ src/dataset_download.py
@@ -6,9 +6,12 @@ from botocore.client import BaseClient
 from botocore.config import Config
 from src.helpers import GEOPARQUET_DIR, BUCKET_NAME, REGION_NAME, QUARTERS
 
-# configuring the logger/error handling
+# Configure logging (less noise from botocore)
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+logging.getLogger("botocore").setLevel(
+    logging.WARNING
+)  # Suppress botocore checksum spam
 
 # making the directory
 os.makedirs(GEOPARQUET_DIR, exist_ok=True)
@@ -22,15 +25,15 @@ def create_s3_client() -> BaseClient:
 
 
 # creating the download file function and creating the loggers
-def prepare_download(
-    s3_client: BaseClient, s3_key: str, local_file_path: str = GEOPARQUET_DIR
-) -> None:
+def prepare_download(s3_client: BaseClient, s3_key: str) -> None:
+    filename = os.path.basename(s3_key)
+    local_file_path = os.path.join(GEOPARQUET_DIR, filename)
     if os.path.exists(local_file_path):
         logger.info(f"File {local_file_path} already exists. Skipping download.")
         return
     logger.info(f"Downloading s3://{BUCKET_NAME}/{s3_key} to {local_file_path}")
     try:
-        s3_client.prepare_download(BUCKET_NAME, s3_key, local_file_path)
+        s3_client.download_file(BUCKET_NAME, s3_key, local_file_path)
         logger.info(f"Successfully downloaded {local_file_path}")
     except Exception as e:
         logger.error(f"Error downloading s3://{BUCKET_NAME}/{s3_key}: {e}")
@@ -40,6 +43,9 @@ def prepare_download(
 def download_files() -> None:
     """
     Downloads the performance data files from the Ookla S3 bucket to a local directory
+    Note: there is a similarity in the naming convention download_file and download_files
+    I can't do anything to change it becuase that's just what the botocore requires so just
+    be aware of the differences
     """
     s3_client = create_s3_client()
     formats = ["shapefiles", "parquet"]
@@ -52,5 +58,4 @@ def download_files() -> None:
                     filename = f"{year}-{month}-01_performance_{service_type}_tiles"
                     filename += ".zip" if format_type == "shapefiles" else ".parquet"
                     s3_key = f"{format_type}/performance/type={service_type}/year={year}/quarter={quarter}/{filename}"
-                    local_file_path = os.path.join(GEOPARQUET_DIR, filename)
-                    prepare_download(s3_client, s3_key, local_file_path)
+                    prepare_download(s3_client, s3_key)
