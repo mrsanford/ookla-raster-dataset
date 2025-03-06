c9eefc9b3f4f74869cef9f7446830265b7c60ad7
Michelle Sanford <mrsanford@wm.edu>
2025-03-05 23:49:23 -0500

    added docstrings and typehints for optional geopackage function

--- src/make_geopackage.py
+++ src/make_geopackage.py
@@ -9,6 +9,11 @@ logger = logging.getLogger(__name__)
 
 
 def make_geopackage(gdf: gpd.GeoDataFrame, output_path: Path):
+    """
+    Saves a GeoDataFrame as a GeoPackage (.gpkg)
+    Note: this is not a crucial step to obtaining raster output
+    and is just an option for additional visualization functionality
+    """
     try:
         if gdf is not None and isinstance(gdf, gpd.GeoDataFrame):
             gdf.to_file(output_path, driver="GPKG")
