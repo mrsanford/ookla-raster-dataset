3d87bb0586fcd62cb2c255ccabfc32e51249057c
Michelle Sanford <mrsanford@wm.edu>
2025-04-10 15:10:55 -0400

    getting rid of one_band_pipeline

--- main.py
+++ main.py
@@ -11,43 +11,15 @@ from src.helpers import (
     BAND16_COLS,
     BAND32_COLS,
 )
-from src.transform_populate import (
-    read_parquet,
-    create_band_array,
-)
+from src.transform_populate import read_parquet
 from src.generate_raster import (
     make_raster_profile,
-    write_single_band_raster,
     write_multiband_raster_chunks,
 )
 
 # Logging setup
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
-
-
-def full_one_band_pipeline(
-    parquet_path: str = os.path.join(GEOPARQUET_DIR, TEST_PARQUET_FILE),
-    band_column: str = "avg_u_kbps",
-) -> None:
-    """
-    Entire pipeline will read parquet files -> build arrays -> write raster output files
-    Available functionalities: make geopackages from parquets
-    Incoming functionalities: manually clearing memory storage after array creation and writing to memory
-    allowing different dtypes for different columns
-    """
-    gdf = read_parquet(parquet_path)
-    if gdf is None:
-        logger.error("GeoDataFrame loading failed. Aborting raster generation.")
-        return
-    band_array = create_band_array(gdf, band_column)
-    profile = make_raster_profile(num_bands=5, grid_size=GRID_SIZE)
-    write_single_band_raster(band_array, profile)
-    # cleanup memory
-    # del gdf, array_data
-    # gc.collect()
-
-    logger.info("Raster pipeline complete.")
 
 
 def full_multiband_ookla_pipeline(
